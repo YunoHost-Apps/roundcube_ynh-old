8bb510887ad978e91ba675669aad0f6d24f77363
Kload <kload@kload.fr>
2014-05-28 22:19:37 +0200

    [fix] Add automatic_addressbook conf

--- conf/main.inc.php
+++ conf/main.inc.php
@@ -915,4 +915,19 @@ $rcmail_config['autocomplete_single'] = false;
 // Georgia, Helvetica, Impact, Tahoma, Terminal, Times New Roman, Trebuchet MS, Verdana
 $rcmail_config['default_font'] = 'Verdana';
 
+/* automatic_addressbook plugin */
+$rcmail_config['default_addressbook'] = '0';
+
+/* on edit move to default addressbook */
+$rcmail_config['on_edit_move_to_default'] = FALSE;
+
+/* database table name */
+$rcmail_config['db_table_collected_contacts'] = 'collected_contacts';
+
+/* Is automatic_addressbook enabled or disabled by default for users ? */
+$rcmail_config['use_auto_abook'] = TRUE;
+
+/* Should automatic_addressbook be used for completion ? */
+$rcmail_config['use_auto_abook_for_completion'] = TRUE;
+
 // end of config file
